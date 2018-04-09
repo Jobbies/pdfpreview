703d39b9bcabb67c3b0c52cbc89c8e118a695a4f
Robert Katzki <robert@katzki.de>
2018-04-09 21:14:55 +0200

    feat: update preview generation
    It didn’t work so I fixed the generation to work again in Kirby 2.

--- pdfpreview.php
+++ pdfpreview.php
@@ -1,29 +1,27 @@
 <?php
-
-function preview($file, $size)
-{
+file::$methods['pdfPreview'] = function($file, $size='800') {
     $key  = 'pdfpreview_'. md5($file->root());
-    $thumb = kirby()->roots()->thumbs() .DS. $key;
+    $thumb = kirby()->roots()->content() .DS. $key .'.jpg';
+    $thumbUrl = '/' . $key .'.jpg';
 
-    if (f::exists($thumb))
-    {
+    if (f::exists($thumb)) {
         if ( (f::modified($thumb) > $file->modified())) {
-            return kirby()->urls()->thumbs() .'/'. $key;
+            return image($thumbUrl);
         }
         f::remove($thumb);
     }
 
     $image = new Imagick($file->root().'[0]'); // first page only
-    $image->setColorspace(COLORSPACE_RGB);
+    $image->setImageColorspace(Imagick::COLORSPACE_RGB);
     $image->setImageBackgroundColor('white');
-    $image->setImageAlphaChannel(11);
     $image->mergeImageLayers(Imagick::LAYERMETHOD_FLATTEN);
     $image->setImageFormat('jpg');
     $image->setImageCompression(Imagick::COMPRESSION_JPEG);
     $image->setImageCompressionQuality(80);
     $image->thumbnailImage($size, 0);
-    $image->borderImage('black', 1, 1);
     $image->writeImage($thumb);
+    $image->clear();
+    $image->destroy();
 
-    return kirby()->urls()->thumbs() .'/'. $key;
+    return image($thumbUrl);
 };
